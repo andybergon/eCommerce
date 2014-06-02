fa054119d8d7d14fcd59ed55ee5a3da1c8b1b167
andybergon <andybergon@gmail.com>
2014-06-02 03:21:07 +0200

    Initial Commit

---
+++ eCommerce/src/it/uniroma3/model/ProductFacade.java
@@ -0,0 +1,47 @@
+package it.uniroma3.model;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaQuery;
+
+@Stateless
+public class ProductFacade {
+
+	@PersistenceContext(unitName = "unit-jee-es2")
+	private EntityManager em;
+
+	public Product createProduct(String name, String code, Float price, String description) {
+		Product product = new Product(name, price, description, code);
+		this.em.persist(product);
+		return product;
+	}
+
+	public Product getProduct(Long id) {
+		Product product = this.em.find(Product.class, id);
+		return product;
+	}
+
+	public List<Product> getAllProducts() {
+		CriteriaQuery<Product> cq = this.em.getCriteriaBuilder().createQuery(Product.class);
+		cq.select(cq.from(Product.class));
+		List<Product> products = this.em.createQuery(cq).getResultList();
+		return products;
+	}
+
+	public void updateProduct(Product product) {
+		this.em.merge(product);
+	}
+
+	private void deleteProduct(Product product) {
+		this.em.remove(product);
+	}
+
+	public void deleteProduct(Long id) {
+		Product product = this.em.find(Product.class, id);
+		this.deleteProduct(product);
+	}
+
+}
